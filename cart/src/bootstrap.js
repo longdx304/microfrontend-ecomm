96a92c2acff77319943a20f563f4f995e1818c9b
Do Xuan Long <longdo@dobtech.vn>
2023-07-16 22:58:23 +0700

    refactor to account for unpredictable element name in container

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,5 +1,20 @@
 import faker from 'faker';
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+const mount = (el) => {
+  const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
 
-document.querySelector('#dev-cart').innerHTML = cartText;
+  el.innerHTML = cartText;
+};
+
+// If in development mode and running in isolation
+// Assume that only this MFE have an element with id dev-cart
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#dev-cart');
+
+  if (el) {
+    mount(el);
+  }
+}
+
+// Export mount for container to use
+export { mount };
--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,27 @@
 import faker from 'faker';
 
-let products = '';
+const mount = (el) => {
+  let products = '';
 
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+
+  el.innerHTML = products;
+  // if using React
+  // ReactDOM.render(<App />, el);
+};
+
+// If in development mode and running in isolation
+// Assume that only this MFE have an element with id dev-products
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#dev-products');
+
+  if (el) {
+    mount(el);
+  }
 }
 
-document.querySelector('#dev-products').innerHTML = products;
+// Export mount for container to use
+export { mount };
